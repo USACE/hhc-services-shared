131a0108788984d614769412f5327a16cc53e865
Jeff Gregory <jeffrey.s.gregory@me.com>
2025-04-21 08:57:05 -0500

    restructure local dev for postgis

--- services/migration/main/V1_05__TIGER_County.bash
+++ services/migration/main/V1_05__TIGER_County.bash
@@ -7,5 +7,5 @@ unzip -o tl_2024_us_county.zip
 shp2pgsql -s 4269 -c -D -w tl_2024_us_county tiger_data.county >/tmp/tiger_county.sql
 
 # tiger data is gis_admin and I am hhc schema
-PGPASSWORD=${FLYWAY_PLACEHOLDERS_GIS_PASSWORD} psql --host=${FLYWAY_DB_HOST} --port=5432 --username=gis_admin --dbname=${FP__flyway_database__} \
+PGPASSWORD=${FLYWAY_PASSWORD} psql --host=${FLYWAY_DB_HOST} --port=5432 --username=${FP__flyway_user__} --dbname=${FP__flyway_database__} \
     --file="/tmp/tiger_county.sql"
