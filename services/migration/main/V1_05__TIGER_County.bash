f36be32c2479b39eb0d122bb9952cae1cb3672c7
Jeff Gregory <jeffrey.s.gregory@me.com>
2025-09-12 09:31:31 -0500

    migration script updates

--- services/migration/main/V1_05__TIGER_County.bash
+++ services/migration/main/V1_05__TIGER_County.bash
@@ -4,7 +4,7 @@ cd $OPT_MIGRATION
 
 tar -xzvf tl_2024_us_county.tar.gz
 
-shp2pgsql -s 4269:4326 -c -D -w tl_2024_us_county tiger_data.county >/tmp/tiger_county.sql
+shp2pgsql -s 4269:4326 -d tl_2024_us_county tiger_data.county >/tmp/tiger_county.sql
 
 # tiger data is gis_admin and I am hhc schema
 PGPASSWORD=${FLYWAY_PASSWORD} psql --host=${PGHOST} --port=5432 --username=${FLYWAY_USER} --dbname=${FP__flyway_database__} \
--- services/migration/main/V1_06_01__WBDHU6.bash
+++ services/migration/main/V1_06_01__WBDHU6.bash
@@ -4,7 +4,7 @@ cd $OPT_MIGRATION
 
 tar -xzvf wbdhu6.tar.gz
 
-shp2pgsql -s 4326 -c -D -w wbdhu6 hhc.wbdhu6 >/tmp/wbdhu6.sql
+shp2pgsql -s 4326 -d wbdhu6 hhc.wbdhu6 >/tmp/wbdhu6.sql
 
 # tiger data is gis_admin and I am hhc schema
 PGPASSWORD=${FLYWAY_PASSWORD} psql --host=${PGHOST} --port=5432 --username=${FLYWAY_USER} --dbname=${FP__flyway_database__} \
